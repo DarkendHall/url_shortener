b0b2b5027816b3b812251d00a1fbd531aba48cda
DarkendHall <marcus.leeman@iths.se>
2022-05-06 22:55:16 +0200

    Add annotation: Pattern

--- src/main/java/org/darkend/url_shortener/entity/Url.java
+++ src/main/java/org/darkend/url_shortener/entity/Url.java
@@ -3,11 +3,15 @@ package org.darkend.url_shortener.entity;
 import io.micronaut.core.annotation.Introspected;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Introspected
 public class Url {
 
     @NotBlank
+    @Pattern(regexp = "https?:\\/\\/(www\\.[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{2,3}(\\/.[a-zA-Z0-9/]*)?)?" +
+            "([a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{2,3}(\\.[a-zA-Z0-9()]{2,3})?([a-zA-Z0-9/]*))?", message =
+            "You need to provide a valid url")
     private String url;
 
     public Url() {
