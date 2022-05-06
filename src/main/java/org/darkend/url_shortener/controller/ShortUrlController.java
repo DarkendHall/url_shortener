26971229e125aa88dd013111c93c59e49581f575
DarkendHall <marcus.leeman@iths.se>
2022-05-06 12:12:48 +0200

    Update controller to be compatible as native image

--- src/main/java/org/darkend/url_shortener/controller/ShortUrlController.java
+++ src/main/java/org/darkend/url_shortener/controller/ShortUrlController.java
@@ -6,6 +6,7 @@ import org.darkend.url_shortener.service.ShortUrlService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,31 +16,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping
 public class ShortUrlController {
 
+    private final String HOST_URL;
+
     private final Logger logger = LoggerFactory.getLogger(ShortUrlController.class);
     private final ShortUrlService service;
 
     public ShortUrlController(ShortUrlService service) {
         this.service = service;
+        this.HOST_URL = System.getenv("HOST_URL");
+        checkHostUrl();
     }
 
     @ResponseBody
     @PostMapping("short")
     @ResponseStatus(HttpStatus.CREATED)
-    public ShortUrl createNewShortUrl(@RequestBody @Valid Url originalUrl, HttpServletResponse response,
-                                      HttpServletRequest request) {
-        var requestUri = request.getRemoteHost() + ":" + request.getLocalPort();
-        ShortUrl savedUrl = service.createShortUrl(originalUrl, requestUri);
-        response.addHeader("Location", savedUrl.getShortenedUrl());
-        return savedUrl;
+    public ResponseEntity<ShortUrl> createNewShortUrl(@RequestBody @Valid Url originalUrl) {
+        ShortUrl savedUrl = service.createShortUrl(originalUrl, HOST_URL);
+        return ResponseEntity.created(URI.create(savedUrl.getShortenedUrl()))
+                .body(savedUrl);
     }
 
     @ResponseBody
@@ -50,12 +52,20 @@ public class ShortUrlController {
 
     @GetMapping("s/{id}")
     @ResponseStatus(HttpStatus.FOUND)
-    public void getShortUrl(@PathVariable String id, HttpServletResponse response) {
+    public ResponseEntity<Void> getShortUrl(@PathVariable String id) {
         logger.debug("Received GET request, getting redirect url");
         var normalUrl = service.getShortUrl(id)
                 .getNormalUrl();
         logger.debug("Redirect gotten");
         logger.debug("Starting redirect");
-        response.addHeader("Location", normalUrl);
+        return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT)
+                .header("Location", normalUrl)
+                .build();
+    }
+
+    private void checkHostUrl() {
+        if (this.HOST_URL == null) {
+            throw new IllegalStateException("Environment variable 'HOST_URL' not set");
+        }
     }
 }
