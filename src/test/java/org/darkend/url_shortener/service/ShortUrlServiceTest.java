31696afd1074bf36bfa6003a0b37cad2cb03a76c
MarcusLeeman <marcus.leeman@iths.se>
2022-05-11 09:05:16 +0200

    Add more tests for ShortUrlServiceTest

--- src/test/java/org/darkend/url_shortener/service/ShortUrlServiceTest.java
+++ src/test/java/org/darkend/url_shortener/service/ShortUrlServiceTest.java
@@ -7,12 +7,14 @@ import org.darkend.url_shortener.utility.IdGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -25,19 +27,21 @@ class ShortUrlServiceTest {
 
     private ShortUrlService service;
     private ShortUrlRepository repository;
+    private Validator validator;
 
     @BeforeEach
     void setUp() {
-        shortUrl = new ShortUrl("123abc", "http:///s/123abc", "test.test");
+        shortUrl = new ShortUrl("123abc", "http://localhost.com/s/123abc", "http://localhost.com");
 
         repository = mock(ShortUrlRepository.class);
-        Validator validator = mock(Validator.class);
+        validator = mock(Validator.class);
         IdGenerator idGenerator = mock(IdGenerator.class);
         service = new ShortUrlService(repository, validator, idGenerator);
 
         when(validator.validate(any())).thenReturn(Set.of());
         when(repository.findAll()).thenReturn(List.of(shortUrl));
         when(repository.findById("123abc")).thenReturn(Optional.of(shortUrl));
+        when(repository.findById("1234bc")).thenReturn(Optional.empty());
         when(idGenerator.generateId()).thenReturn("123abc");
         when(repository.save(any(ShortUrl.class))).thenAnswer(invocationOnMock -> {
             Object[] args = invocationOnMock.getArguments();
@@ -48,10 +52,19 @@ class ShortUrlServiceTest {
     @Test
     void createShortUrlShouldCreateAndSaveShortUrl() {
         when(repository.findById("123abc")).thenReturn(Optional.empty());
-        var result = service.createShortUrl(new Url("test.test"), "");
+        var result = service.createShortUrl(new Url("http://localhost.com"), "localhost.com");
 
         assertThat(result).isEqualTo(shortUrl);
         verify(repository, times(1)).save(result);
+    }
+
+    @Test
+    void createShortUrlWithInvalidUrlShouldThrowException() {
+        when(repository.findById("123abc")).thenReturn(Optional.empty());
+        when(validator.validate(any())).thenThrow(ConstraintViolationException.class);
+        assertThatThrownBy(() -> service.createShortUrl(new Url("http://localhost"), "localhost.com"))
+                .isExactlyInstanceOf(ConstraintViolationException.class);
+        verify(repository, times(1)).findById("123abc");
     }
 
     @Test
@@ -60,6 +73,13 @@ class ShortUrlServiceTest {
 
         assertThat(result).isEqualTo(shortUrl);
         verify(repository, times(1)).findById("123abc");
+    }
+
+    @Test
+    void getShortUrlWithInvalidIdShouldThrowException() {
+        assertThatThrownBy(() -> service.getShortUrl("1234bc"));
+
+        verify(repository, times(1)).findById("1234bc");
     }
 
     @Test
