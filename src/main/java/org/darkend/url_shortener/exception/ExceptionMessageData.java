2c77407533472b6fda0441a10e0ed6587622e722
DarkendHall <marcus.leeman@iths.se>
2022-05-19 12:09:48 +0200

    Convert record to class

--- src/main/java/org/darkend/url_shortener/exception/ExceptionMessageData.java
+++ src/main/java/org/darkend/url_shortener/exception/ExceptionMessageData.java
@@ -1,4 +1,31 @@
 package org.darkend.url_shortener.exception;
 
-public record ExceptionMessageData(String timestamp, int status, String message) {
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.annotation.ReflectiveAccess;
+
+@Introspected
+@ReflectiveAccess
+public class ExceptionMessageData {
+
+    private final String timestamp;
+    private final int status;
+    private final String message;
+
+    public ExceptionMessageData(String timestamp, int status, String message) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
